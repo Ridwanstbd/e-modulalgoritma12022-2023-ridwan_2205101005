3d6fee31425ad92674d059b63d662a13a6f5e5b1
Ridwan Setio Budi <108109278+Ridwanstbd@users.noreply.github.com>
2022-10-09 19:39:01 +0700

    mengerjakan latihan 2.4
    Saya Telah Selesai Mengerjakan Latihan 2.4

---
+++ latihan2.4.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+main(){
+//	mendeklarasikan variabel
+	const char mother[]= "ALCE", father[]="JACK", 
+	bigBrother[]="ERIC", litteBrother[]="FADIL", iam[]="'ME'";
+//	akhir dari mendeklarasikan variabel 
+//	menjalankan perintah dan output my familiy
+	cout<< "MY FAMILIY: \n" << "Hello everyone ... ";
+	cout<< "I want to introduction for my family. First, my mother \n";
+	cout<< "Is "<<mother<<" and my father is "<< father << endl;
+	cout<< "I Have two family , ones my big brother is "<<bigBrother<<" and My little brother is "
+	<<litteBrother<<", So I am is "<<iam<<endl;
+//	akhir dari menjalankan perintah dan output my familiy
+	return 0;
+}
